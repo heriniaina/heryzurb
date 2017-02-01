9db5db2bb5e301cea58f3b5e93888480dfe786a9
Hery <hery@serasera.org>
2017-02-01 06:34:11 +0100

    Fixing VARIABLE

--- template.php
+++ template.php
@@ -1,16 +1,6 @@
 <?php
 
 
-/**
- * Implements template_preprocess_page
- *
- * Add convenience variables and template suggestions.
- */
-function heryzurb_preprocess_page(&$variables) {
-  //custom css for the site
-  $variables['heryzurb_css'] = '';
-  $variables['heryzurb_css'] = theme_get_setting('heryzurb_css');
-}
 
 /**
  * Custom search block form
